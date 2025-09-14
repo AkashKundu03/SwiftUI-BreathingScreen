d725d0d9dfb7015e94cfe06361874a2c55521f1b
Akash Kundu <akashkundu03work@gmail.com>
2025-09-14 19:56:27 +0530

    Initial Commit

---
+++ edgeglow/edgeglowApp.swift
@@ -0,0 +1,17 @@
+//
+//  edgeglowApp.swift
+//  edgeglow
+//
+//  Created by Akash Kundu on 14/09/25.
+//
+
+import SwiftUI
+
+@main
+struct edgeglowApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ edgeglowTests/edgeglowTests.swift
@@ -0,0 +1,17 @@
+//
+//  edgeglowTests.swift
+//  edgeglowTests
+//
+//  Created by Akash Kundu on 14/09/25.
+//
+
+import Testing
+@testable import edgeglow
+
+struct edgeglowTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
